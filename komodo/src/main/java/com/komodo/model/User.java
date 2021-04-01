7869558c102f42b9ec4a2cc3fb7ccc03998d24ab
Hans Gehman <thehansgehman@gmail.com>
2021-04-01 17:40:46 -0500

    Hans added code to the Model User Class from Exeter Java Front-End Integration Lesson 3

--- komodo/src/main/java/com/komodo/model/User.java
+++ komodo/src/main/java/com/komodo/model/User.java
@@ -1,10 +1,19 @@
 //Here is a comment to test if Hans got the repo cloned properly with java in vscode
 //Testing wing's origin
+
+//What Hans added to this came from Exeter Java Front-End Integration Lesson 3
 package com.komodo.model;
 
+import javax.persistence.*;
+import java.lang.invoke.InjectedProfile;
+
+@Entity
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(nullable = false, unique = true)
 	private String username;
 	
 	private String email;
