bfd24d2173f7e75e19e7009d402b6dd7df4e1348
wing <wongwingchit@gmail.com>
2021-04-02 11:08:06 +0800

    commiting on dev

--- komodo/src/main/java/com/komodo/repository/UserRepository.java
+++ komodo/src/main/java/com/komodo/repository/UserRepository.java
@@ -1,6 +1,8 @@
 //What Hans added to this came from Exeter Java Front-End Integration Lesson 3
 package com.komodo.repository;
 
+import com.komodo.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
