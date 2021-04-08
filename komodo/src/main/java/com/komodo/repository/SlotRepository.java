ba9cc2a946351a3b2d7871be767c53ca425250a9
wing <wongwingchit@gmail.com>
2021-04-08 15:50:22 +0800

    implimented slot repository in appointment controller

--- komodo/src/main/java/com/komodo/repository/SlotRepository.java
+++ komodo/src/main/java/com/komodo/repository/SlotRepository.java
@@ -1,10 +1,17 @@
 package com.komodo.repository;
 
+import java.util.Date;
+
 import com.komodo.model.Slot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SlotRepository extends JpaRepository<Slot,Integer> {
     Slot findById(Long id);
+    Slot findByDay(Date date);
     
+    
+    // Slot findBySlot_3AndDay(Long slot_3, Date date);
+    // Slot findBySlot_2AndDay(Long slot_2, Date date);
+    // Slot findBySlot_1AndDay(Long slot_1, Date date);
 }
